871c8f6a008f1023a3b8e5a6e0eba588cf6d94f1
Sheewi <shelbyewing53@gmail.com>
2025-08-26 04:44:36 -0500

    Add IDX configuration for cloud development environment

---
+++ .idx/dev.nix
@@ -0,0 +1,61 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs_20
+    pkgs.nodePackages.npm
+    pkgs.maven
+    pkgs.openjdk21
+    pkgs.firebase-tools
+  ];
+
+  # Sets environment variables in the workspace
+  env = {
+    JAVA_HOME = "${pkgs.openjdk21}/lib/openjava";
+  };
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "bradlc.vscode-tailwindcss"
+      "ms-vscode.vscode-typescript-next"
+      "ms-vscode.vscode-json"
+      "toba.vsfire"
+    ];
+
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        # Configure previewss individually (optional)
+        web = {
+          # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+          # and show it in IDX's web preview panel
+          command = ["npm" "run" "dev"];
+          manager = "web";
+          env = {
+            # Environment variables to set for your server
+            PORT = "$PORT";
+          };
+        };
+      };
+    };
+
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        npm-install = "npm install --legacy-peer-deps";
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        # Example: start a background task to watch and re-build backend code
+        # watch-backend = "npm run watch-backend";
+      };
+    };
+  };
+}
